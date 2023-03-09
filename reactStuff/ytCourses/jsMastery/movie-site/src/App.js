720763c6be8bad135e6e9d01201f16ac4d0cf470
Chase <cnickellsumn@gmail.com>
2023-03-09 07:39:44 -0800

    learned how to use props
    pass in (props) as argument for function,
    then can assign with jsx later on when you do <Component />
    
    you'd be like name={'asdf} in the jsx area
    
    but above when you're actually writing what the function Component
    should return, it'd be <div>Name: {props.name}</div> for example

--- reactStuff/ytCourses/jsMastery/movie-site/src/App.js
+++ reactStuff/ytCourses/jsMastery/movie-site/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+// https://www.youtube.com/watch?v=b9eMGE7QtTk&list=PL6QREj8te1P6wX9m5KnicnDVEucbOPsqR&index=10&t=1745s
+
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Last Name: {props.lastName}</h2>
+      <h2>Age: {props.age}</h2>
+    </>
+  )
+}
+
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name={'test'} lastName={'lastname'} age={25}/>
+      <Person />
     </div>
   );
 }
