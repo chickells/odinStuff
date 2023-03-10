cabf2c8a05813bee1302a661fdf6ea98c1219500
Chase <cnickellsumn@gmail.com>
2023-03-10 10:07:54 -0800

    learned how useEffect works but lets be honest, I HAVE NO FUCKING IDEA WHAT IT ACTUALLY DOES
    I guess it just calls every time the page refreshes or some shit??
    idk, going to watch more vidoes

--- reactStuff/ytCourses/jsMastery/movie-site/src/App.js
+++ reactStuff/ytCourses/jsMastery/movie-site/src/App.js
@@ -1,24 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 // https://www.youtube.com/watch?v=b9eMGE7QtTk&list=PL6QREj8te1P6wX9m5KnicnDVEucbOPsqR&index=10&t=1745s
 
-const Person = (props) => {
-  return (
-    <>
-      <h1>Name: {props.name}</h1>
-      <h2>Last Name: {props.lastName}</h2>
-      <h2>Age: {props.age}</h2>
-    </>
-  )
-}
-
-
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    setCounter(100)
+  }, [])
+  
   return (
     <div className="App">
-      <Person name={'test'} lastName={'lastname'} age={25}/>
-      <Person />
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }
