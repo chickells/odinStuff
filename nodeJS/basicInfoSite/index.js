3d2e3dcb5ca55dc5257d675fe826e056df4ddcc8
Chase <cnickellsumn@gmail.com>
2023-04-06 07:26:06 -0700

    SENDING BACK MULTIPLE LINES HTML REQUIRES MULTIPLE RES.WRITE() LINES FOR EACH LINE OF HTML

--- nodeJS/basicInfoSite/index.js
+++ nodeJS/basicInfoSite/index.js
@@ -3,7 +3,16 @@ const fs = require('fs');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    log('request made')
+    log(req.url, req.method);
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write(
+        // can't use line breaks??? it always breaks the code
+            '<h1>Home page Welcome to the home page! this is generally where youd find a lot more information for a landing page.  Lets pretend this is a shop for racing gear.</h1>'
+        )
+    res.end();
 })
 
 server.listen(1337, 'localhost', () => {
