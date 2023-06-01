013c91f1606a4be281e10c52a6f2cc29c0d1e565
Chase <cnickellsumn@gmail.com>
2023-06-01 14:15:35 -0700

    trying to integrate a call too google places api

--- Django/soloProjects/djangoReact-v1/frontend-react/src/App.js
+++ Django/soloProjects/djangoReact-v1/frontend-react/src/App.js
@@ -4,13 +4,16 @@ import './App.css';
 function App() {
     const [zipCode, setZipCode] = useState('');
     const [showOutput, setShowOutput] = useState(false);
-    const [newZipCode, setNewZipCode] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
       e.preventDefault();
       setShowOutput(true);
-      setNewZipCode(zipCode);
+      
+      const response = await fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants&location=${zipCode}&radius=4828.03&key=AIzaSyCp4-mmKHxrs1OCIgd4y_qOERMgzPA2wIc`);
+      const data = await response.json();
+      console.log(data);
     }
+      // https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants&location=90018&radius=4828.03&key=AIzaSyCp4-mmKHxrs1OCIgd4y_qOERMgzPA2wIc 
 
     return (
       <div className="center-container">
@@ -23,12 +26,11 @@ function App() {
             onChange={(e) => setZipCode(e.target.value)} 
             className="input"
           />
-          <button type="submit" className="button">HELLO SHUFFLE</button>
+          <button type="submit" className="button">SHUFFLE</button>
         </form>
         {showOutput && (
           <div className="output">
-            <p>Your entered zip code is:</p>
-            <p className="zip">{newZipCode}</p>
+            
           </div>
         )}
       </div>
