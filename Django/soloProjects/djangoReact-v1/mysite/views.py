66f6aec11f642bfd63bac0a46f827589c05fd921
Chase <cnickellsumn@gmail.com>
2023-06-02 11:00:08 -0700

    updated views.py to get zip_code param from URL

--- Django/soloProjects/djangoReact-v1/mysite/views.py
+++ Django/soloProjects/djangoReact-v1/mysite/views.py
@@ -2,10 +2,11 @@ import requests
 from django.http import JsonResponse
 
 def google_maps_proxy(request):
+    zip_code = request.GET.get('zip_code') # Get the zip_code parameter from the request -> `../?zip_code=${zipCode}`, where zipCode is from the App.js file
     url = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
     params = {
         'query': 'restaurants',
-        'location': '$90018',
+        'location': f'{zip_code}', # Use the zip_code variable in the request
         'radius': '4828.03',
         'key': 'AIzaSyCp4-mmKHxrs1OCIgd4y_qOERMgzPA2wIc'
     }
